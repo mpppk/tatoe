ba324ffb5800216a05d685630eef98459a294f0b
mpppk <niboshiporipori@gmail.com>
2021-05-02 16:14:46 +0900

    Add material ui

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -10,18 +10,32 @@ import {
 import { ErrorBoundary } from "react-error-boundary"
 import LoginForm from "app/auth/components/LoginForm"
 
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import React from "react"
+
+//You can customize this as you want and even move it out to a separate file
+const theme = createMuiTheme({
+  palette: {
+    type: "light",
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
   const router = useRouter()
 
   return (
-    <ErrorBoundary
-      FallbackComponent={RootErrorFallback}
-      resetKeys={[router.asPath]}
-      onReset={useQueryErrorResetBoundary().reset}
-    >
-      {getLayout(<Component {...pageProps} />)}
-    </ErrorBoundary>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ErrorBoundary
+        FallbackComponent={RootErrorFallback}
+        resetKeys={[router.asPath]}
+        onReset={useQueryErrorResetBoundary().reset}
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </ErrorBoundary>
+    </ThemeProvider>
   )
 }
 
