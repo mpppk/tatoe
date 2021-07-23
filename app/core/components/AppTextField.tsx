65830802e12cca692841c869ecb8095fa3073592
mpppk <niboshiporipori@gmail.com>
2021-07-23 17:36:57 +0900

    Fix placeholder overlap

--- app/core/components/AppTextField.tsx
+++ app/core/components/AppTextField.tsx
@@ -1,6 +1,15 @@
 import { TextField, TextFieldProps } from "mui-rff"
 import React from "react"
+import { useField } from "react-final-form"
 
 export const AppTextField: React.FC<TextFieldProps> = (props) => {
-  return <TextField margin={"dense"} variant={"outlined"} {...props} />
+  const { input, meta } = useField(props.name)
+  return (
+    <TextField
+      InputLabelProps={{ shrink: input.value !== "" || meta.active }}
+      margin={"dense"}
+      variant={"outlined"}
+      {...props}
+    />
+  )
 }
