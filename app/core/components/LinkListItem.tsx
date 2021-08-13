6ed21be64f1d09852aa055b74930b8e83dfc7648
okashige <okashige.masatoshi@g.wakayama-u.jp>
2021-08-13 12:31:10 +0900

    [0814#デザイン修正] Top, ランキング作成画面のレイアウトを修正

--- app/core/components/LinkListItem.tsx
+++ app/core/components/LinkListItem.tsx
@@ -1,16 +1,24 @@
 import { Link, LinkProps } from "blitz"
-import { Link as MUILink, ListItem, ListItemText } from "@material-ui/core"
+import { Link as MUILink, ListItem, ListItemText, makeStyles } from "@material-ui/core"
 import React from "react"
 
 type Props = {
   text: string
 } & LinkProps
 
+const useStyles = makeStyles((_theme) => ({
+  linkListItem: {
+    borderBottom: "1px solid #dcdcdc",
+    padding: _theme.spacing(1),
+  },
+}))
+
 export const LinkListItem: React.FC<Props> = (props) => {
+  const classes = useStyles()
   return (
     <Link href={props.href}>
       <MUILink href="#">
-        <ListItem>
+        <ListItem className={classes.linkListItem}>
           <ListItemText primary={props.text} color={"primary"} />
         </ListItem>
       </MUILink>
