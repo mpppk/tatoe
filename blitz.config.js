47da164b438f86d4e88102c659019d5147b27874
niboshi <niboshiporipori@gmail.com>
2021-09-05 06:05:15 +0900

    Add Cache-Control header (#186)
    * Add Cache-Control header
    
    * Fix source

--- blitz.config.js
+++ blitz.config.js
@@ -1,11 +1,32 @@
 const { sessionMiddleware, simpleRolesIsAuthorized } = require("blitz")
 
+const cacheHeader = {
+  key: "Cache-Control",
+  value: "public, max-age3600",
+}
+
 module.exports = {
   middleware: [
     sessionMiddleware({
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [cacheHeader],
+      },
+      {
+        source: "/rankings/:ranking*",
+        headers: [cacheHeader],
+      },
+      {
+        source: "/users/:user*",
+        headers: [cacheHeader],
+      },
+    ]
+  },
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
